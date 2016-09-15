0fd58fe339e71ca89428228a01089167d6ae19aa
lordke <kejiecd@gmail.com>
2016-09-15 16:56:55 +0800

    mongodb数据库连接模块及配置信息

--- mongodb.py
+++ mongodb.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
+#引入pymongo 模块 定义基本数据库配置
 MongoClient=MongoClient('localhost',27017)
-db=MongoClient.blog
+db=MongoClient.blog   #数据库下的blogcollection
 collections=db.articles
 weibouser=db.weibouser
 fzweibouser=db.fzweibouser
