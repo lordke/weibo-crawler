ecfdbcac647a44ef48b0ece1b8e21aece3fe8d5f
lordke <kejiecd@gmail.com>
2016-09-15 17:01:02 +0800

    用户信息module

--- userinfo.py
+++ userinfo.py
@@ -2,17 +2,17 @@ import mongodb
 
 class Userinfo(object):
     def __init__(self,uid,name,sex,address,intro,artsum,follows,fans,ifmember,ifv,vintro=''):
-        self.uid=uid
-        self.name=name
-        self.sex=sex
-        self.address=address
-        self.intro=intro
-        self.artsum=artsum
-        self.follows=follows
-        self.fans=fans
-        self.ifmember=ifmember
-        self.ifv=ifv
-        self.vintro=vintro
+        self.uid=uid  #用户uid
+        self.name=name  #用户姓名
+        self.sex=sex  #性别
+        self.address=address   #地址
+        self.intro=intro  # 介绍
+        self.artsum=artsum # 总微博数
+        self.follows=follows  #关注人数
+        self.fans=fans  # 粉丝人数
+        self.ifmember=ifmember # 是否微博会员
+        self.ifv=ifv # 是否加V
+        self.vintro=vintro #  加V认证介绍
         self.data={
             'uid':self.uid ,
             'name':self.name,
@@ -27,7 +27,7 @@ class Userinfo(object):
             'vintro':self.vintro
         }
 
-    def save(self):
+    def save(self):   #用户信息操作方法
         mongodb.qwweibouser.insert_one(self.data)
 
     def find(self):
